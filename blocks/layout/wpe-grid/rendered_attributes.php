686daf25836b5a10deee68e932357f3ff6f7b3d0
Paul Balanche <pb@buzzbrothers.ch>
2022-09-15 17:20:53 +0200

    Up

--- blocks/layout/wpe-grid/rendered_attributes.php
+++ blocks/layout/wpe-grid/rendered_attributes.php
@@ -3,8 +3,8 @@
 // Define data
 $data = [
     'content' => $content,
-    'margin' => ( isset($attributes['margin']) && is_array($attributes['margin']) ) ? implode(' ', array_map( function ($v, $k) { if( strpos($k, 'm') == 0 ) { return $k . '-' . $v; } }, $attributes['margin'], array_keys($attributes['margin']) )) : '',
-    'padding' => ( isset($attributes['padding']) && is_array($attributes['padding']) ) ? implode(' ', array_map( function ($v, $k) { if( strpos($k, 'p') == 0 ) { return $k . '-' . $v; } }, $attributes['padding'], array_keys($attributes['padding']) )) : ''
+    'margin' => ( is_array($attributes) && isset($attributes['margin']) && is_array($attributes['margin']) ) ? implode(' ', array_map( function ($v, $k) { if( strpos($k, 'm') == 0 ) { return $k . '-' . $v; } }, $attributes['margin'], array_keys($attributes['margin']) )) : '',
+    'padding' => ( is_array($attributes) && isset($attributes['padding']) && is_array($attributes['padding']) ) ? implode(' ', array_map( function ($v, $k) { if( strpos($k, 'p') == 0 ) { return $k . '-' . $v; } }, $attributes['padding'], array_keys($attributes['padding']) )) : ''
 ];
 
 return $data;
