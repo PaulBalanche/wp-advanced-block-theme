e7cfd331f15841e6b2c536e5bd37e9c43d1f8b8b
Paul Balanche <pb@buzzbrothers.ch>
2023-05-24 17:35:44 +0200

    1.4.1

--- wp-advanced-block-theme.php
+++ wp-advanced-block-theme.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: 		WP advanced block theme
- * Version: 			1.4.0
+ * Version: 			1.4.1
  * Requires at least: 	6.0
  * Requires PHP:      	8.0
  * Author: 				Paul Balanche
