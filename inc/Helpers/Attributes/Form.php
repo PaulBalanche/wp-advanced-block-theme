841fdde985529ccd812bb1736835dd078cdc588c
Paul Balanche <pb@buzzbrothers.ch>
2023-09-08 12:11:50 +0200

    Up

--- inc/Helpers/Attributes/Form.php
+++ inc/Helpers/Attributes/Form.php
@@ -12,43 +12,11 @@ class Form extends Base {
     public static function format( &$propInstance ) {
 
         if( is_numeric( $propInstance->getValue() ) ) {
-    
-            $formData = [
-                'action' => '/',
-                'lnf' => [
-                    'labelType' => 'block'
-                ],
-                'fields' => [],
-                'actions' => [
-                    [
-                        'type' => 'submit',
-                        'label' => 'Send my message!'
-                    ]
-                ]
-            ];
+            $formattedForm = apply_filters( 'Abt\attribute_format_form', $propInstance->getValue() );
 
-            $form = get_post($propInstance->getValue());
-
-            $parser_class = apply_filters( 'block_parser_class', 'WP_Block_Parser' );
-            $parser = new $parser_class();
-            $fields_parsed = $parser->parse( $form->post_content );
-            if( is_array($fields_parsed) ) {
-                foreach( $fields_parsed as $field ) {
-                    if( $field['blockName'] == 'custom/wpe-contact-form-field' ) {
-                        $formData['fields'][] = [
-                            'type' => $field['attrs']['type'],
-                            'label' => $field['attrs']['label'],
-                            'name' => $field['attrs']['label'],
-                            'placeholder' => $field['attrs']['label'],
-                        ];
-                    }
-                }
+            if( is_array($formattedForm) ) {
+                return $formattedForm;
             }
-
-            return $formData;
-            echo '<pre>';
-            print_r($formData);
-            die;
         }
 
         return null;
